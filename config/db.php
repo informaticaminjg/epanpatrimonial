a09072c6dba446978ef079c02f1bec88f62688f9
informaticaminjg <informaticaminjg@gmail.com>
2026-09-02 12:49:52 -0300

    sdatabase

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 return [
         'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=192.168.24.62;dbname=mjg',
-    'username' => 'pyuser',
-    'password' => 'pass123',
+    'username' => 'root',
+    'password' => 'x1fifIVANE',
     'charset' => 'utf8',
 ];
 
