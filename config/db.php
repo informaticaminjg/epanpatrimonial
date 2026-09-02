a955627a815c765f706d114a964be9b63f275b74
informaticaminjg <informaticaminjg@gmail.com>
2026-09-02 13:19:57 -0300

    databesuser2

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 return [
         'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=192.168.24.62;dbname=mjg',
-    'username' => 'root',
-    'password' => 'x1fifIVANE',
+    'username' => 'pyuser2',
+    'password' => '31zIru',
     'charset' => 'utf8',
 ];
 
